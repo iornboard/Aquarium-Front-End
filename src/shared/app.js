0228b8abbedb53bb833f2453816fba637d1de1b6
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-02-21 12:33:52 +0900

    nam - 21.02.21

---
+++ src/shared/app.js
@@ -0,0 +1,14 @@
+import { Route } from 'react-router-dom';
+import { Home, About } from '../pages';
+
+function App() {
+
+  return (
+    <div>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+    </div>
+  );
+}
+
+export default App;
