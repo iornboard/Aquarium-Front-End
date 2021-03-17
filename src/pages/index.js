3d5aec0bfd2cba51e47b9a1d4fce24956969fda0
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-03-17 13:40:54 +0900

    devLog 21.03.17

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,2 +1,4 @@
-export { default as Home } from './Home';
 export { default as About } from './About';
+export { default as Login } from './TestLogin';  
+export { default as SignUp } from './TestSignUp';
+export { default as Main } from './TestMain';
--- src/shared/app.js
+++ src/shared/app.js
@@ -1,16 +1,44 @@
 import { Route, Switch } from 'react-router-dom';
-import { Home, About } from '../pages';
+import { About , Login, SignUp , Main } from '../pages';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#ff8346',
+      light: '#ff9b6b',
+      dark: '#b25b31'
+    },
+    secondary: {
+      main: '#3c2822'
+    },
+    error: {
+      main: '#ff1744'
+    },
+    text: {
+      primary: '#ff8346',
+      secondary: '#3c2822',
+      disabled: '#4caf50',
+      hint: '#ffc107',
+      myTextColor: '#039be5'
+    }
+  },
+})
 
 function App() {
 
   return (
     <div>
-        <Route exact path="/" component={Home}/>
-        <Switch>
-          <Route path="/about/:name" component={About}/>  {/* :키 로하는 params를 새로 생성한다. */}
-          <Route path="/about" component={About}/>
-        </Switch>
-        {/* 리엑트 라우터에서 <switch> 컴포넌트를 통해서 중복되는 라우터중에서 하나(맨 위에 있는거)만 표현가능하다*/}
+       <ThemeProvider theme={theme} style={{ padding: '20px' }}>
+          <Route exact path="/" component={Main}/>
+          <Switch>
+            <Route path="/about/:name" component={About}/>  {/* :키 로하는 params를 새로 생성한다. */}
+            <Route path="/about" component={About}/>
+            <Route path="/Login" component={Login}/>
+            <Route path="/SignUp" component={SignUp}/>
+          </Switch>
+          {/* 리엑트 라우터에서 <switch> 컴포넌트를 통해서 중복되는 라우터중에서 하나(맨 위에 있는거)만 표현가능하다*/}
+        </ThemeProvider>
     </div>
   );
 }
