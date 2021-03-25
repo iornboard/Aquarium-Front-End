bfa89504445725f6a755b40212345d045ec2ae2c
growingtree-code <tjsruddks12@gmail.com>
2021-03-25 15:55:46 +0900

    create page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 export { default as About } from './About';
-export { default as Login } from './TestLogin';  
-export { default as SignUp } from './TestSignUp';
-export { default as Main } from './TestMain';
-export { default as Sun } from './TestSun';
+export { default as Login } from './test/TestLogin';  
+export { default as SignUp } from './test/TestSignUp';
+export { default as Main } from './test/TestMain';
+export { default as Sun } from './test/TestSun';
