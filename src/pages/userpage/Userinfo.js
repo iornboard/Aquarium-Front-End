81d64d33daee051626eef8af557e17127a7e2fcd
growingtree-code <tjsruddks12@gmail.com>
2021-04-04 02:12:53 +0900

    Userinfo page middle save

--- src/pages/userpage/Userinfo.js
+++ src/pages/userpage/Userinfo.js
@@ -6,9 +6,15 @@ import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import AppBar from '../../components/AppBarMain';
 import Scroll from '../../components/ListMain';
-
-import Avatar from '@material-ui/core/Avatar'; 
-//아바타
+import Avatar from '@material-ui/core/Avatar';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+//아바타+텍스트필드
 
 //------------이거삭제해보기
 function Copyright() {
@@ -29,10 +35,11 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
+    minHeight: '60vh',
+    minHeight: '150vh'
   },
   main: {
-    marginTop: theme.spacing(8),
+    marginTop: theme.spacing(10),
     marginBottom: theme.spacing(2),
   },
   footer: {
@@ -66,6 +73,7 @@ const useStyles = makeStyles((theme) => ({
   cardContent: {
     flexGrow: 1,
   },
+  //Website 2021부분
   footer: {
     backgroundColor: theme.palette.background.paper,
     padding: theme.spacing(6),
@@ -75,10 +83,28 @@ const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     '& > *': {
-      margin: theme.spacing(1),
+      margin: theme.spacing(5), // 컨테이너테두리에서 안쪽 사이에 공간의 크기
     },
+  
+  },
+  large: {
+    width: theme.spacing(10),
+    height: theme.spacing(10),
   },
 //--아바타
+//텍스트필드
+  root: {
+    '& > *': {
+      margin: theme.spacing(5),
+      width: '25ch',
+    },
+  },
+  //수정및 저장버튼
+  button: {
+    margin: theme.spacing(1),
+  },
+  
+
 }));
 
 
@@ -89,16 +115,26 @@ export default function StickyFooter() {
   return (
     <React.Fragment>
     <div className={classes.root}>
-
-      <Avatar alt="Remy Sharp" src="/static/images/avatar/1.jpg" />
-      <Avatar alt="Travis Howard" src="/static/images/avatar/2.jpg" />
-      <Avatar alt="Cindy Baker" src="/static/images/avatar/3.jpg" />
-
       <CssBaseline />
+      
       <Container component="main" className={classes.main} maxWidth="sm">
-        <Typography variant="h2" component="h1" gutterBottom>
-          Sticky footer
+        {/* 해당경로에 사진추가 및 위치변경하기 */}
+      <Avatar alt="Aquarium" src="/static/images/avatar/1.jpg" className={classes.large}/>
+
+        <Typography variant="h5" component="h6" gutterBottom>
+          내 정보 수정
         </Typography>
+{/* https://material-ui.com/components/text-fields/#text-field */}
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="Email Address" />
+          <TextField id="outlined-basic" label="Password" />
+          <TextField id="outlined-basic" label="userFullname" />
+          <TextField id="outlined-basic" label="userNickname" />
+        </form>
+
+        <Button variant="contained"  color="primary"size="large"className={classes.button}startIcon={<SaveIcon />}>
+          수정 및 저장
+        </Button>
         <Typography variant="h5" component="h2" gutterBottom>
           {'Pin a footer to the bottom of the viewport.'}
           {'The footer will move as the main element of the page grows.'}
