11e5117f92777f6973ac31c94a97ab32f482ba3d
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-05-20 00:29:47 +0900

    devlog -m by man

--- src/components/TaskTool.js
+++ src/components/TaskTool.js
@@ -3,33 +3,35 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import Card from '@material-ui/core/Card';
-import Button from '@material-ui/core/Button';
-import { Height } from '@material-ui/icons';
 
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
-    minHeight: '100vh',
+    minHeight: '80vh',
   },
   testSection1: {
     marginTop: theme.spacing(1),
     background : '#2ECCFA',
     width : '100%', 
-    minHeight: '50vh',
+    minHeight: '40vh',
+    maxHeight: '50vh',
+
   },
   testSection2 : {
     marginTop: theme.spacing(1),
     background : '#2ECCFA',
     width : '100%', 
-    minHeight: '30vh',
+    minHeight: '20vh',
+    maxHeight: '30vh',
   },
   testSection3 : {
     marginTop: theme.spacing(1),
     background : '#2ECCFA',
     width : '100%', 
-    minHeight: '15vh',
+    minHeight: '9.5vh',
+    maxHeight: '14.5vh',
   },
 }));
 
--- src/components/TasktBar.js
+++ src/components/TasktBar.js
@@ -1,15 +1,24 @@
 import React, { useEffect } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
 import Typography from '@material-ui/core/Typography';
 import LinearProgress from '@material-ui/core/LinearProgress';
 import Box from '@material-ui/core/Box';
 import AvatarComp from './AvatarComp'
 import Chip from '@material-ui/core/Chip';
 import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TaskTool from './TaskTool'
 
 const useStyles = makeStyles((theme) => ({
     root: {
+      marginTop: theme.spacing(1),
+      width : '100%' 
+    },
+    layout: {
       marginTop: theme.spacing(1),
       background : '#2ECCFA',
       width : '100%' 
@@ -35,7 +44,7 @@ const useStyles = makeStyles((theme) => ({
     },
     taskbar: {
         marginLeft: theme.spacing(1),
-    }
+    },
   }));
 
 
@@ -58,6 +67,15 @@ export default function FloatingActionButtons(props) {
   
   const [progress, setProgress] = React.useState(0);
   const [dDay, setdDay] = React.useState(0);
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
 
   useEffect(() => {
@@ -82,13 +100,11 @@ export default function FloatingActionButtons(props) {
   
  
 
-
   return (
     <div className={classes.root}>
 
-        <Card className={classes.root}>
-
-          <Grid container className={classes.root} spacing={2}>
+        <Button variant="contained" color="primary" onClick={handleClickOpen} disableElevation className={classes.layout} >
+          <Grid container spacing={2}>
 
             <Grid item xs={4} className={classes.taskbar} >      
               <Box display="flex" alignItems="center" className={classes.taskbar}>
@@ -117,8 +133,30 @@ export default function FloatingActionButtons(props) {
             </Grid>
             
           </Grid>
+        </Button>
 
-        </Card>
+
+        <Dialog
+          fullWidth
+          maxWidth='lg'
+          open={open}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+        <DialogTitle id="alert-dialog-title">{taskName}</DialogTitle>
+        <DialogContent>
+
+          <TaskTool/>
+
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary" autoFocus>
+            저장
+          </Button>
+        </DialogActions>
+
+      </Dialog>
+
 
     </div>
   );
