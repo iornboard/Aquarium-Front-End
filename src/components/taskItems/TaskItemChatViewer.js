ffb71f65f235ea301269fe930acebeee1f8ee2de
DESKTOP-U2P2DON\KUCK_SU <kuck1106@naver.com>
2021-06-04 23:18:21 +0900

    devlog 21.06.05 by nam

--- src/components/taskItems/TaskItemChatViewer.js
+++ src/components/taskItems/TaskItemChatViewer.js
@@ -7,10 +7,34 @@ export default  function Example() {
     showChatbox: true,
     messages: [
       {
-        text: 'user2 has joined the conversation',
-        timestamp: 1578366389250,
-        type: 'notification',
-      }
+        author: {
+          username: 'user1',
+          id: 1,
+          avatarUrl: 'http://www.foodnmed.com/news/photo/201706/6987_1287_1511.jpg',
+        },
+        text: 'Hi',
+        type: 'text',
+        timestamp: 1578366393250,
+      },
+      {
+        author: { username: 'user2', id: 2, avatarUrl: null },
+        text: 'Show two buttons',
+        type: 'text',
+        timestamp: 1578366425250,
+        buttons: [
+          {
+            type: 'URL',
+            title: 'Yahoo',
+            payload: 'http://www.yahoo.com',
+          },
+          {
+            type: 'URL',
+            title: 'Example',
+            payload: 'http://www.example.com',
+          },
+        ],
+      },
+      
     ],
   });
 
