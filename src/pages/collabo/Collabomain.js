8d027f90888db37537a5522a6f1451f14fa3bb67
growingtree-code <tjsruddks12@gmail.com>
2021-06-10 16:24:17 +0900

    210610 collamain change

--- src/pages/collabo/Collabomain.js
+++ src/pages/collabo/Collabomain.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
 import TaskBarViewer  from '../../components/tasks/TaskBarViewer'
-import TaskScheduleViewer  from '../../components/tasks/TaskBarViewer'
-
+import TaskScheduleViewer  from '../../components/tasks/TaskScheduleViewer'
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
 
 function Copyright() {
   return (
@@ -19,7 +25,6 @@ function Copyright() {
     </Typography>
   );
 }
-
 
 const taskOne = {
   taskName : "기말고사",
@@ -37,17 +42,47 @@ const taskOne = {
   userName : "남현수",
 }
 
-const currentDate = '2018-11-01';
-const schedulerData = [
-  { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
-  { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
-];
+// 수정
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`full-width-tabpanel-${index}`}
+      aria-labelledby={`full-width-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `full-width-tab-${index}`,
+    'aria-controls': `full-width-tabpanel-${index}`,
+  };
+}
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: 'flex',
     flexDirection: 'column',
     minHeight: '100vh',
+    backgroundColor: theme.palette.background.paper,
+    width: 500,
   },
   list: {
     marginTop: theme.spacing(8),
@@ -72,26 +107,62 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-
-export default function StickyFooter() {
+export default function FullWidthTabs() {
   const classes = useStyles();
-
+  const theme = useTheme();
   const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
+  const handleChangeIndex = (index) => {
+    setValue(index);
+  };
+
   return (
     <div className={classes.root}>
+      
+      <AppBar position="static" color="default">
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="fullWidth"
+          aria-label="full width tabs example"
+        >
+          <Tab label="Management" {...a11yProps(0)} />
+          <Tab label="schedule" {...a11yProps(1)} />
+        </Tabs>
+      </AppBar>
+      
 
-      <CssBaseline />
-
-      <TaskBarViewer />  {/* 작업 모음 리스트 뷰어 */}
-
-      <TaskScheduleViewer/> {/* 작업 스케쥴 리스트 뷰어 */}
-   
-    
+      <SwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={value}
+        onChangeIndex={handleChangeIndex}
+      >
+        <div className={classes.root}>
+          <CssBaseline />
+          <TabPanel value={value} index={0} dir={theme.direction}>
+            <TaskBarViewer />  {/* 작업 모음 리스트 뷰어 */}
+          </TabPanel>
+          <TabPanel value={value} index={1} dir={theme.direction}>
+            <TaskScheduleViewer/> {/* 작업 스케쥴 리스트 뷰어 */}
+          </TabPanel>
+        </div>
+      </SwipeableViews>
+      
     </div>
+    
   );
+  
 }
+  
+  const currentDate = '2018-11-01';
+  const schedulerData = [
+    { startDate: '2018-11-01T09:45', endDate: '2018-11-01T11:00', title: 'Meeting' },
+    { startDate: '2018-11-01T12:00', endDate: '2018-11-01T13:30', title: 'Go to a gym' },
+  ];
+
