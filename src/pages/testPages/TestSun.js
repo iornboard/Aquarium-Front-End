bf0c8db74b0361fe5cad8ae3bde1dad91ebaf08f
growingtree <tjsruddks12@gmail.com>
2021-10-16 19:23:51 +0900

    testsun change bysun

--- src/pages/testPages/TestSun.js
+++ src/pages/testPages/TestSun.js
@@ -27,6 +27,7 @@ import Avatar from '@material-ui/core/Avatar';
 import Divider from '@material-ui/core/Divider';
 
 import AquariumYT from '../../components/aquarium/AquariumYoutube';
+import { readAllAquarium } from '../../_actions/actionAquarium'//*
 
 function Copyright() {
   return (
@@ -48,7 +49,7 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     // 변경
     '& > *': {
-      margin: theme.spacing(1),
+      // margin: theme.spacing(1),
     },
   },
   toolbar: {
@@ -60,21 +61,6 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'flex-end',
     padding: '0 8px',
     ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
   },
   menuButton: {
     marginRight: 36,
@@ -116,8 +102,11 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(4),
   },
   paper: {
-    padding: theme.spacing(2),
     display: 'flex',
+    minWidth: '60vw',
+    minHeight: '100%',
+
+    padding: theme.spacing(2),
     overflow: 'auto',
     flexDirection: 'column',
     float: 'left',
@@ -154,26 +143,38 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
+
 function Mypage({match, userInfo, history}) {
+
   const classes = useStyles();
   const dispatch = useDispatch();
   const theme = useTheme();
 
-  const [open, setOpen] = React.useState(true);
-  
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };
+  const [aquariums, setAquariums ] = React.useState();   
+
+  const [pageUserInfo, setPageUserInfo] = React.useState();
+
+  const {userNickname, userId, userImgUrl, userEmail, createdAt} = {...pageUserInfo} 
+
 
   
    //!!!하드코딩!!!
   useEffect(() => {
+    // url의 사용자 이름을 서버한테 물어봐서 있으면 정보를 가져오고, 없으면 로그인 페이지로 보내버림
     dispatch(authUserPage(match.params.username))
-    .then(res => { res ? history.push("/")  : console.log(res) })
+      .then(res => { res ? setPageUserInfo(res.payload.data) : history.push("/") })
+
   }, 1);
+
+
+  useEffect(() => {
+    // 페이지의 유저 아이디를 서버한테 보내서, 모든 아쿠아리움 정보를 받아로는 로직
+    if(userId != null){
+      dispatch(readAllAquarium(userId))
+      .then(res => setAquariums(res.payload))
+    }
+  }, [userId]);
+
 
   //!!!하드코딩!!!
 
@@ -189,23 +190,23 @@ function Mypage({match, userInfo, history}) {
           <Grid container>
             <Grid spacing={3} className={classes.root} item xs={4}>
               <Box className={classes.boxx}>
-              <Avatar alt="userimg" src="" className={classes.large} />
+              <Avatar alt="userimg" src={userImgUrl} className={classes.large} />
                 <br/>
-                <Divider style={{margin : "10px 0px 10px 0px" , height: 3, backgroundColor: theme.palette.primary.main,}}/>
+                <Divider style={{margin : "10px 0px 10px 0px" , height: 3, backgroundColor: theme.palette.primary.main}}/>
 
               <Box textAlign="center" m={1}>
                 <Typography variant="h4" gutterBottom>
-                  {"사용자이름"}
+                  {userNickname}
                 </Typography>
               </Box>
 
               <Box textAlign="left" m={1}>
                 <Typography variant="subtitle1" gutterBottom>
-                  <strong>{"이메일:"}</strong>
+                  <strong>{"이메일: " + userEmail}</strong>
                 </Typography>
                 
                 <Typography variant="subtitle1" gutterBottom>
-                  <strong>{"가입일자:"}</strong>
+                  <strong>{new Date(createdAt).toLocaleDateString('ko-KR', { year: 'numeric',month: 'long', day: 'numeric',}) + "일 가입됨"}</strong>
                 </Typography>
               </Box>
               
@@ -217,7 +218,7 @@ function Mypage({match, userInfo, history}) {
            
             <Grid item xs={8}>
               <Paper className={classes.paper}>
-                <WorkCard />
+                { aquariums ? <WorkCard aqrms={aquariums}/> : <NoCard/> }
               </Paper>
             </Grid>
             {/* <Grid item xs={12} md={8} lg={9}>
@@ -259,9 +260,26 @@ function Mypage({match, userInfo, history}) {
   );
 }
 
-const cards = [1, 2, 3, 4, 5, 6];
 
-const WorkCard = () => {
+const WorkCard = ({aqrms}) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+    <Container className={classes.cardGrid}>
+          {/* End hero unit */}
+          <Grid container spacing={4}>
+            {aqrms.map( Aq => (//키+값을 묶어서 하나씩 매핑시켜 넣어주는 코드
+               <AqrmCard aqrms={Aq}/>
+            ))}
+          </Grid>
+    </Container>
+    </div>
+  );
+}
+
+
+const AqrmCard = ({aqrms}) => {
   const classes = useStyles();
 
   const [open, setOpen] = React.useState(false);
@@ -275,44 +293,42 @@ const WorkCard = () => {
   };
 
   return (
-    <div>
-    <Container className={classes.cardGrid}>
-          {/* End hero unit */}
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card onClick={handleClickOpen}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="../logo512.png"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe the content.
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-    </Container>
 
-    <Dialog onClose={handleClose}  maxWidth={"xl"} open={open} >
+    <Grid item xs={12} sm={6} md={4}>
+      <Card onClick={handleClickOpen}>
+         <CardMedia
+          className={classes.cardMedia}
+          image="../logo512.png"
+          title="Image title"
+          />
+        <CardContent className={classes.cardContent}>
+           <Typography gutterBottom variant="h5" component="h2">
+            {aqrms.aqrmTitle}
+          </Typography>
+          {/* <Typography>
+             {aqrms.aqrmLikeCount}
+           </Typography> */}
+         </CardContent>
+      </Card>
 
-        <AquariumYT aqrmId={1}  className={classes.rootAquarium} />
+      <Dialog onClose={handleClose}  maxWidth={"xl"} open={open} >
+        <AquariumYT aqrmId={aqrms.aqrmId}  className={classes.rootAquarium} />
+      </Dialog>
+    </Grid>
 
-    </Dialog>
-
-
-
-
-    </div>
   );
 }
+
+const NoCard = () => {
+  const classes = useStyles();
+  return (
+      <Box display = "flex" justifyContent = "center" alignItems="center">
+          <img src = "../logo512.png" width = "40%"/>
+          <h1>No Contents</h1>
+      </Box>
+  );
+}
+
 
 
 export default withRouter(Mypage)
